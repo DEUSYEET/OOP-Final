c44c9e48333779510c289a0427b0bd64e377e404
Ragger15 <carsonsena@gmail.com>
2019-02-28 13:55:39 -0700

    added serial ID to Score

--- src/models/Score.java
+++ src/models/Score.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class Score implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int score;
 	private String name;
 	private static Score[] highscores;
