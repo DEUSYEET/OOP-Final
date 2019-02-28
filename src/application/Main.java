8e6c747c84789ac515d6004401a310092b569ffc
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2019-02-28 13:52:07 -0700

    Added Sprite Class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,47 +1,33 @@
 package application;
 
-import assets.Animation;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	Animation a = new Animation("fire");
+
+	BorderPane root;
+	Sprite s = new Sprite(100, 100, 32, 32, "Player", "idle");
 	double t = 0;
-	GraphicsContext gc;
-	// ImageView img = new ImageView(a.getCurrentFrame());
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 
-			BorderPane root = new BorderPane();
+			root = new BorderPane();
 			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 
-			Canvas c = new Canvas(400, 400);
-			root.getChildren().add(c);
+			root.getChildren().add(s);
 
-			gc = c.getGraphicsContext2D();
-
-			int i2 = 0;
-			for (Image i : a.getFrames()) {
-				gc.drawImage(i, 32*i2, 50);
-				i2++;
-			}
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
-			
-
-			
-			
 			timer.start();
 
 		} catch (Exception e) {
@@ -53,15 +39,20 @@ public class Main extends Application {
 		launch(args);
 	}
 
+	private List<Sprite> sprites() {
+		return root.getChildren().stream().map(n -> (Sprite) n).collect(Collectors.toList());
+	}
+
 	private void update() {
-		t += 1;
-		System.out.println(t);
-		if (t > 10) {
-			a.nextFrame();
-			gc.clearRect(0, 0,45,45);
-			gc.drawImage(a.getCurrentFrame(), 10, 10);
-			t = 0;
+		t++;
+		if (t > 2) {
+			for (Sprite s : sprites()) {
+				s.update();
+			}
+			
+			t=0;
 		}
+
 	}
 
 	AnimationTimer timer = new AnimationTimer() {
---
+++ src/application/Sprite.java
@@ -0,0 +1,42 @@
+package application;
+
+import assets.Animation;
+import javafx.scene.image.ImageView;
+
+class Sprite extends ImageView{
+	boolean oofed = false;
+	final String type;
+	Animation animation;
+	
+	Sprite(int x, int y, int w, int h, String type, String SpriteFile) {
+		super();
+		this.type = type;
+		animation = new Animation(SpriteFile);
+		setTranslateX(x);
+		setTranslateY(y);
+		setImage(animation.getCurrentFrame());
+	}
+
+
+
+	public void update() {
+		animation.nextFrame();
+		setImage(animation.getCurrentFrame());
+	}
+	
+	void moveLeft() {
+		setTranslateX(getTranslateX() - 5);
+	}
+
+	void moveRight() {
+		setTranslateX(getTranslateX() + 5);
+	}
+
+	void moveUp() {
+		setTranslateY(getTranslateY() - 5);
+	}
+
+	void moveDown() {
+		setTranslateY(getTranslateY() + 5);
+	}
+}
