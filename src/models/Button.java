e5a46a2b485ef6ac4343b29888610c5bcd47dabf
Beck <bes6896@yahoo.com>
2019-02-28 15:00:50 -0700

    Somee More Formatting

--- src/models/Button.java
+++ src/models/Button.java
@@ -4,11 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
 public class Button {
-	
+
 	public String text;
 	public Stage whereGoOnClick;
-	
+
 	public void onClick(ActionEvent event) {
-		
+
 	}
 }
--- src/models/Freeze.java
+++ src/models/Freeze.java
@@ -2,16 +2,16 @@ package models;
 
 import enums.PowerUps;
 
-public class Freeze extends PowerUp{
+public class Freeze extends PowerUp {
 
 	public Freeze(int x, int y) {
-		int[] pos = {x,y};
-		
+		int[] pos = { x, y };
+
 		setPosition(pos);
 		this.type = PowerUps.FREEZE;
-		
+
 	}
-	
+
 	@Override
 	public void onUse(Player player) {
 		player.setCurrentPowerUp(this);
