ffc8e63c36264d46f7a4abace7ad783b4f72a08e
Ragger15 <carsonsena@gmail.com>
2019-02-28 13:40:38 -0700

    do over

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,60 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private Label bulb = new Label();
+	private boolean isOn = false;
+	private final String ON = "-fx-background-color: firebrick";
+	private final String OFF = "-fx-background-color: dodgerblue";
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			VBox root = new VBox();
+			root.setAlignment(Pos.CENTER);
+			
+			bulb.setMinSize(340, 240);
+			bulb.setStyle(OFF);
+			
+			Button lightSwitch = new Button("Don't Touch");
+			lightSwitch.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					isOn = !isOn;
+					
+					bulb.setStyle(isOn ? ON : OFF);
+				}
+				
+			});
+			
+			VBox switchBox = new VBox();
+			switchBox.setAlignment(Pos.CENTER);
+			switchBox.setPadding(new Insets(20,80,20,80));
+			switchBox.getChildren().add(lightSwitch);
+			
+			root.getChildren().addAll(bulb,switchBox);
+			
+			Scene scene = new Scene(root, 400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Deme");
+			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
