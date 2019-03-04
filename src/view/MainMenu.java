39f45f7a51436da70db5e6eb27e3658a3e4edca5
DEUSYEET <47902658+DEUSYEET@users.noreply.github.com>
2019-03-04 14:00:04 -0700

    Updated sprite sizes
    new sizes in SpriteSizes.txt

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,20 +1,12 @@
 package view;
 
-import java.awt.Graphics;
-import java.awt.Image;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainMenu {
@@ -49,16 +41,11 @@ public class MainMenu {
 			
 		});
 		
-        
-		BackgroundFill background = new BackgroundFill(Color.BLACK, new CornerRadii(1), null);
 		VBox optionsBox = new VBox();
-	
 		optionsBox.setAlignment(Pos.CENTER);
 		optionsBox.setPadding(new Insets(20, 80, 20, 80));
 		optionsBox.getChildren().add(singlePlayer);
-		optionsBox.setBackground(new Background(background));
-		optionsBox.setMinHeight(100000000);
-		
+
 		root.getChildren().addAll(optionsBox);
 		
 		
