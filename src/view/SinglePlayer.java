1588fdd26f7e0a9190dd988c666fd8592783cf25
troy82086 <turner82086@gmail.com>
2019-03-06 13:36:58 -0700

    getting rid of a unused import

--- src/view/SinglePlayer.java
+++ src/view/SinglePlayer.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import models.Player;
 
